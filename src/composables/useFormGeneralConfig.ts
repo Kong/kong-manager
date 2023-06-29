5e9f06970c0424e1716767664471af9e2be3c096
Yi Yang <Leopoldthecoder@users.noreply.github.com>
2023-06-29 16:20:31 +0800

    fix(*): remove workspace from API endpoints (#1)

--- src/composables/useFormGeneralConfig.ts
+++ src/composables/useFormGeneralConfig.ts
@@ -4,7 +4,7 @@ import type { KongManagerBaseFormConfig } from '@kong-ui/entities-shared'
 export const useFormGeneralConfig = () => {
   return reactive({
     app: 'kongManager' as const,
-    workspace: 'default',
+    workspace: '',
     apiBaseUrl: 'http://localhost:8001', // TODO: use the actual Admin API URL instead of hard-coding it
   } as Pick<KongManagerBaseFormConfig, 'app' | 'workspace' | 'apiBaseUrl'>)
 }
--- src/composables/useListGeneralConfig.ts
+++ src/composables/useListGeneralConfig.ts
@@ -4,7 +4,7 @@ import type { KongManagerBaseTableConfig } from '@kong-ui/entities-shared'
 export const useListGeneralConfig = () => {
   return reactive({
     app: 'kongManager' as const,
-    workspace: 'default',
+    workspace: '',
     apiBaseUrl: 'http://localhost:8001', // TODO: use the actual Admin API URL instead of hard-coding it
   } as Pick<KongManagerBaseTableConfig, 'app' | 'workspace' | 'apiBaseUrl'>)
 }
