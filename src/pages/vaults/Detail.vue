b4825a7932ff70e68edbf16546fad3701720f58c
Xiaoyan Rao <270668624@qq.com>
2023-07-03 14:31:10 +0800

    feat(vault): add vault list page

---
+++ src/pages/vaults/Detail.vue
@@ -0,0 +1,38 @@
+<template>
+  <VaultConfigCard
+    :config="vaultDetailConfig"
+    @copy:success="onCopySuccess"
+  />
+</template>
+
+<script setup lang="ts">
+import { computed, reactive } from 'vue'
+import { useRoute } from 'vue-router'
+import { VaultConfigCard } from '@kong-ui/entities-vaults'
+import { useDetailGeneralConfig } from '@/composables/useDetailGeneralConfig'
+import { useCopyEventHandlers } from '@/composables/useCopyEventHandlers'
+import { useI18n } from '@/composables/useI18n'
+
+defineOptions({
+  name: 'VaultDetail',
+})
+
+const route = useRoute()
+const { t } = useI18n()
+
+const id = computed(() => (route.params.id as string) ?? '')
+
+const vaultDetailConfig = reactive({
+  ...useDetailGeneralConfig(),
+  entityId: id.value,
+})
+
+const { onCopySuccess: openToaster } = useCopyEventHandlers()
+
+const onCopySuccess = () => {
+  openToaster({
+    message: t('global.copied'),
+  })
+}
+
+</script>
