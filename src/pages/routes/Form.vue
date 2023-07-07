ace70467bb56b0fab49b5d7c4a4743999f9bd4be
Yi S <ratsunny@gmail.com>
2023-07-07 11:53:22 +0800

    feat(routes): add routes support (#8)

---
+++ src/pages/routes/Form.vue
@@ -0,0 +1,59 @@
+<template>
+  <PageHeader :title="isEditing ? t('entities.route.edit.form.title') : t('entities.route.create.form.title')" />
+  <RouteForm
+    :config="routeFormConfig"
+    :route-id="id"
+    @update="handleUpdate"
+  />
+</template>
+
+<script setup lang="ts">
+import { computed, reactive } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { RouteForm } from '@kong-ui/entities-routes'
+import { useFormGeneralConfig } from '@/composables/useFormGeneralConfig'
+import { useFormRedirectOnCancel, useFormRedirectOnUpdate } from '@/composables/useFormRedirect'
+import { useToaster } from '@/composables/useToaster'
+import { useI18n } from '@/composables/useI18n'
+
+defineOptions({
+  name: 'RouteForm',
+})
+
+const route = useRoute()
+const router = useRouter()
+const toaster = useToaster()
+const { t } = useI18n()
+
+const id = computed(() => (route.params.id as string) ?? '')
+
+const isEditing = computed(() => !!id.value)
+
+const routeOnCancel = useFormRedirectOnCancel(
+  isEditing.value
+    ? { name: 'route-detail', params: { id: id.value } }
+    : { name: 'route-list' }
+)
+
+const routeOnUpdate = useFormRedirectOnUpdate(
+  isEditing.value
+    ? { name: 'route-detail', params: { id: id.value } }
+    : { name: 'route-list' }
+)
+
+const routeFormConfig = reactive({
+  ...useFormGeneralConfig(),
+  cancelRoute: routeOnCancel,
+})
+
+const handleUpdate = (entity) => {
+  router.push(routeOnUpdate)
+  toaster.open({
+    appearance: 'success',
+    message: t(
+      isEditing.value ? 'entities.route.updated' : 'entities.route.created',
+      { name: entity.name ?? entity.id },
+    ),
+  })
+}
+</script>
