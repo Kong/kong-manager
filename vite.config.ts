5165f885ae5f568ce65b176c53309ab11d09ef31
Yi Yang <Leopoldthecoder@users.noreply.github.com>
2023-07-10 17:33:58 +0800

    feat(*): add konnect cta (#12)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,5 +16,12 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     port: 8080,
+  },
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer,
+      ],
+    },
   },
 })
