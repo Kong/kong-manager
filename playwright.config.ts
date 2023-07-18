02ec12ea6ad60a9611436fc2f78f2e654d99161f
Makito <i@maki.to>
2023-07-18 14:47:20 +0800

    chore(ci): testing pipelines [KAG-2102] (#31)
    * chore(ci): testing pipelines
    
    * chore(ci): fix
    
    * chore(ci): fix
    
    * chore(ci): remove the docker login step
    
    * chore(ci): fix matrix usage
    
    * chore(ci): add test helpers, deps, and eslint
    
    * chore(ci): the missing dollar
    
    * chore(ci): remove debug triggers

---
+++ playwright.config.ts
@@ -0,0 +1,26 @@
+import type { PlaywrightTestConfig } from '@playwright/test'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+const config: PlaywrightTestConfig = {
+  reporter: [['junit', { outputFile: './test-results/playwright.xml' }]],
+  testDir: './tests/playwright',
+  retries: 1,
+  workers: 1,
+  use: {
+    actionTimeout: 5000,
+    baseURL: process.env.KM_TEST_GUI_URL ?? 'http://localhost:8080',
+    headless: true,
+    viewport: { width: 1920, height: 1080 },
+    deviceScaleFactor: 1,
+    ignoreHTTPSErrors: true,
+    screenshot: 'only-on-failure',
+    trace: 'retain-on-failure',
+    contextOptions: {
+      permissions: ['clipboard-read', 'clipboard-write'],
+    },
+  },
+}
+
+export default config
---
+++ tests/playwright/base-test.ts
@@ -0,0 +1,80 @@
+import { test as pwTest, type BrowserContext, type Page } from '@playwright/test'
+import config from '@pw-config'
+import { withNavigation } from './commands/withNavigation'
+
+export interface SharedState {
+  context?: BrowserContext | null
+  page?: Page | null
+}
+
+/**
+ * Get an extended Playwright `test` object with predefined fixtures like `context` and `page`.
+ * The `test` object returned can be further extended.
+ *
+ * @param sharedState object to store the shared states like `context` and `page` (pass `null` to disable sharing)
+ * @returns an extended Playwright `test` object
+ */
+const baseTest = (sharedState: SharedState | null = {}) => {
+  const t = pwTest.extend({
+    context: async ({ browser }, use) => {
+      let context: BrowserContext
+      if (sharedState?.context === undefined || sharedState?.context === null) {
+        context = await browser.newContext()
+
+        if (sharedState !== null && sharedState.context !== null) {
+          sharedState.context = context
+        }
+      } else {
+        context = sharedState?.context
+      }
+
+      await use(context)
+    },
+    page: async ({ context }, use) => {
+      let page: Page
+      if (sharedState?.page === undefined || sharedState?.page === null) {
+        page = new Proxy<Page>(await context.newPage(), {
+          get (target, prop: keyof Page) {
+            if (prop === 'goto') {
+              return function (...args: Parameters<Page['goto']>) {
+                const [url, ...restArgs] = args
+                let targetURL: string
+
+                try {
+                  // do not build URL if `url` is already a valid URL
+                  targetURL = new URL(url).href
+                } catch {
+                  targetURL = `${config.use?.baseURL ?? ''}${url}`
+                }
+
+                return withNavigation(target, () => target[prop](targetURL, ...restArgs), { allowSameURL: true })
+              }
+            }
+
+            return target[prop]
+          },
+        })
+
+        if (sharedState !== null && sharedState.page !== null) {
+          sharedState.page = page
+        }
+      } else {
+        page = sharedState?.page
+      }
+
+      await use(page)
+    },
+  })
+
+  // make playwright always run tests from first to last when retry happens
+  t.describe.configure({ mode: 'serial' })
+
+  t.afterAll(async ({ context, page }) => {
+    await page.close()
+    await context.close()
+  })
+
+  return t
+}
+
+export default baseTest
