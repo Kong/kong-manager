04d8a19a9adf6b8ec8bccc1cfc9285dedcf3603e
Leopoldthecoder <leopoldthecuber@gmail.com>
2023-10-20 14:48:58 +0800

    test(*): fix case

--- tests/playwright/commands/clickConfigurationCopy.ts
+++ tests/playwright/commands/clickConfigurationCopy.ts
@@ -2,5 +2,8 @@ import type { Page, Locator } from '@playwright/test'
 import { selectOption } from './selectOption'
 
 export const clickConfigurationCopy = async (pageOrLocator: Page | Locator, entity: string) => {
-  await selectOption(pageOrLocator.locator(`.kong-ui-${entity}-entity-config-card .config-card-actions`), 'json')
+  const configCardLocator = pageOrLocator.locator(`.kong-ui-${entity}-entity-config-card`)
+
+  await selectOption(configCardLocator.locator('.config-card-actions'), 'json')
+  await configCardLocator.getByTestId('k-code-block-copy-button').click()
 }
