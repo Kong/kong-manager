976c0c4b69257e9dc238609e67d297a60b097acd
Xiaoyan Rao <270668624@qq.com>
2023-12-28 11:57:14 +0800

    fix(kconfig): retrieve edition from info instead of from kconfig.js

--- src/composables/useListGeneralConfig.ts
+++ src/composables/useListGeneralConfig.ts
@@ -1,13 +1,15 @@
 import { reactive, toRefs } from 'vue'
-import { config } from 'config'
 import type { KongManagerBaseTableConfig } from '@kong-ui-public/entities-shared'
 import { useBaseGeneralConfig } from './useBaseGeneralConfig'
+import { useInfoStore } from '@/stores/info'
+
+const infoStore = useInfoStore()
 
 export const useListGeneralConfig = () => {
   return reactive({
     ...toRefs(useBaseGeneralConfig()),
     // Kong Gateway OSS only supports exact match and does not support sorting
-    isExactMatch: config.GATEWAY_EDITION === 'community',
-    disableSorting: config.GATEWAY_EDITION === 'community',
+    isExactMatch: infoStore.kongEdition === 'community',
+    disableSorting: infoStore.kongEdition === 'community',
   }) as Pick<KongManagerBaseTableConfig, 'app' | 'workspace' | 'gatewayInfo' | 'isExactMatch' | 'apiBaseUrl' | 'disableSorting'>
 }
