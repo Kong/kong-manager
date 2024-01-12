861e059f3c22f4a3315d8788529ac6651d9281a4
Yi Yang <Leopoldthecoder@users.noreply.github.com>
2024-01-12 15:02:31 +0800

    feat(*): entity forms JSON/YAML preview (#157)

--- src/composables/useFormGeneralConfig.ts
+++ src/composables/useFormGeneralConfig.ts
@@ -1,6 +1,10 @@
+import { reactive, toRefs } from 'vue'
 import type { KongManagerBaseFormConfig } from '@kong-ui-public/entities-shared'
 import { useBaseGeneralConfig } from './useBaseGeneralConfig'
 
 export const useFormGeneralConfig = () => {
-  return useBaseGeneralConfig() as Pick<KongManagerBaseFormConfig, 'app' | 'workspace' | 'gatewayInfo' | 'apiBaseUrl'>
+  return reactive({
+    ...toRefs(useBaseGeneralConfig()),
+    jsonYamlFormsEnabled: true,
+  }) as Pick<KongManagerBaseFormConfig, 'app' | 'workspace' | 'gatewayInfo' | 'apiBaseUrl' | 'jsonYamlFormsEnabled'>
 }
