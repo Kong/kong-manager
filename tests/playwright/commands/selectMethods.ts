c02b001ed6b58ee0e8fb74b14e24270851707674
Leopoldthecoder <leopoldthecuber@gmail.com>
2025-05-13 17:50:15 +0800

    feat(*): adopt the new route form

--- tests/playwright/commands/selectMethods.ts
+++ tests/playwright/commands/selectMethods.ts
@@ -1,17 +1,11 @@
-import { expect, type Page } from '@playwright/test'
+import { type Page } from '@playwright/test'
 
-export const selectMethods = async (page: Page, methods: string[], is_enabled = false) => {
-  if (!is_enabled) {
-    await page.getByTestId('routing-rule-methods').click()
+export const selectMethods = async (page: Page, methods: string[]) => {
+  await page.getByTestId('route-form-methods').getByTestId('multiselect-trigger').click()
+
+  for (const method of methods) {
+    const locator = page.getByTestId('route-form-methods').getByTestId(`multiselect-item-${method}`)
+    await locator.scrollIntoViewIfNeeded()
+    await locator.click()
   }
-
-  await expect(page.locator('.routing-rule-input .methods-input-container')).toBeVisible()
-
-  await Promise.all(methods.map((method) => {
-    return page
-      .locator('.routing-rule-input .methods-input')
-      .filter({ hasText: method })
-      .locator('.k-input-switch')
-      .click()
-  }))
 }
