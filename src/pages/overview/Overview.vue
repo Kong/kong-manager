e35f2fa9f5c70023330431eb8b72c219f5474edf
Feyzullah SARI <feyzullah.sari@jekirdek.com>
2025-09-02 18:52:42 +0300

    remove kong ads

--- src/pages/overview/Overview.vue
+++ src/pages/overview/Overview.vue
@@ -23,50 +23,12 @@
       </ul>
     </KCard>
   </section>
-  <KCard
-    title="Resources"
-    class="resource-card"
-  >
-    <ul class="resource-list">
-      <li
-        v-for="resource in resources"
-        :key="resource.title"
-        class="resource-item"
-      >
-        <a
-          class="resource-link"
-          :href="resource.link"
-          rel="noopener"
-          target="_blank"
-        >
-          <component
-            :is="resource.icon"
-            :color="KUI_COLOR_TEXT_PRIMARY_STRONG"
-          />
-          <div class="resource-info">
-            <span class="resource-title">{{ resource.title }}</span>
-            <span class="resource-description">{{ resource.description }}</span>
-          </div>
-        </a>
-      </li>
-    </ul>
-  </KCard>
-  <KonnectCTA />
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue'
-import {
-  FlagIcon,
-  BrainIcon,
-  PlugIcon,
-  KongIcon,
-} from '@kong/icons'
-import KonnectCTA from '@/components/KonnectCTA.vue'
 import { useI18n } from '@/composables/useI18n'
 import { useInfoStore } from '@/stores/info'
-import { formatVersion } from '@/utils'
-import { KUI_COLOR_TEXT_PRIMARY_STRONG } from '@kong/design-tokens'
 
 defineOptions({
   name: 'ManagerOverview',
@@ -81,7 +43,7 @@ const config = computed(() => ({
   kongEdition: infoStore.kongEdition,
   hostname: infoStore.info.hostname,
 }))
-const version = computed(() => config.value.kongVersion ? `${formatVersion(config.value.kongVersion)}.x` : 'latest')
+
 const info = computed(() => {
   const guiListeners = config.value.admin_gui_listeners
   const nonSslGuiListener = guiListeners?.find?.(listener => !listener.ssl)
@@ -171,32 +133,6 @@ const info = computed(() => {
     ),
   ]
 })
-const resources = computed(() => [
-  {
-    link: `https://docs.konghq.com/gateway/${version.value}`,
-    icon: FlagIcon,
-    title: t('overview.resource.intro.title'),
-    description: t('overview.resource.intro.description'),
-  },
-  {
-    link: `https://docs.konghq.com/gateway/${version.value}/get-started`,
-    icon: BrainIcon,
-    title: t('overview.resource.start.title'),
-    description: t('overview.resource.start.description'),
-  },
-  {
-    link: 'https://docs.konghq.com/hub',
-    icon: PlugIcon,
-    title: t('overview.resource.plugin.title'),
-    description: t('overview.resource.plugin.description'),
-  },
-  {
-    link: 'https://discuss.konghq.com/',
-    icon: KongIcon,
-    title: t('overview.resource.discuss.title'),
-    description: t('overview.resource.discuss.description'),
-  },
-])
 </script>
 
 <style scoped lang="scss">
@@ -204,7 +140,7 @@ $card-spacing: 32px;
 
 .info-container {
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 1fr;
   grid-gap: $card-spacing;
   margin-bottom: $card-spacing;
 }
