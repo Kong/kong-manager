c1296e1801decb9dab36e59e67154ed008e6f82a
Feyzullah Sarı <feyzullah.sari@jekirdek.com>
2025-10-02 09:14:40 +0300

    kconfig added to version control

---
+++ public/kconfig.js
@@ -0,0 +1,7 @@
+window.K_CONFIG = {
+    'ADMIN_GUI_PATH': '/',
+    'ADMIN_API_URL': 'https://adminapi.securesoft.tr',
+    'ADMIN_API_PORT': '8001',
+    'ADMIN_API_SSL_PORT': '',
+    'ANONYMOUS_REPORTS': 'true'
+  }
