3dfc1c25d3fee6fe5250b01343597deebf446733
Leopoldthecoder <leopoldthecuber@gmail.com>
2025-10-17 14:00:06 +0800

    fix(*): update locators

--- tests/playwright/commands/clickEntityListAction.ts
+++ tests/playwright/commands/clickEntityListAction.ts
@@ -4,15 +4,22 @@ export const clickEntityListAction = async (
   pageOrLocator: Page | Locator,
   action: string,
   nth = 1,
+  page?: Page,
 ) => {
+  // if the first argument is a Locator, the fourth argument must be provided
+  if (('blur' in pageOrLocator) && page === undefined) {
+    throw new Error('When the first argument is a Locator, provide a Page object as the last argument')
+  }
+
   const row = pageOrLocator
     .locator('.k-table-data')
     .locator('tr')
     .nth(nth)
 
   await row.locator('[data-testid="row-actions-dropdown-trigger"]').click()
-  await row
-    .locator('[data-testid="dropdown-list"]')
+  await (page ?? pageOrLocator)
+    .locator('body')
+    .locator('[data-testid="dropdown-list"]:visible')
     .locator(`[data-testid="action-entity-${action}"]`)
     .click()
 }
