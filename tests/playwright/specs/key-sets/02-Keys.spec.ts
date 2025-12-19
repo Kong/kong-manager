2526345b55b1bce001a10335d0d775a0cb94b681
Leopoldthecoder <leopoldthecuber@gmail.com>
2025-12-19 11:50:30 +0800

    test(*): keys delete tests

--- tests/playwright/specs/key-sets/02-Keys.spec.ts
+++ tests/playwright/specs/key-sets/02-Keys.spec.ts
@@ -1,5 +1,6 @@
 import { expect } from '@playwright/test'
 import baseTest from '@pw/base-test'
+import { autocompleteDeleteModal } from '@pw/commands/autocompleteDeleteModal'
 import { clearKongResources } from '@pw/commands/clearKongResources'
 import { clickEntityListAction } from '@pw/commands/clickEntityListAction'
 import { createKongResource } from '@pw/commands/createKongResource'
@@ -70,8 +71,7 @@ test.describe('key-set keys tab', () => {
     await withNavigation(page, () => clickEntityListAction(page, 'view'))
     await switchDetailTab(page, 'keys')
     await clickEntityListAction(page, 'delete')
-    await expect(page.locator('.kong-ui-entity-delete-modal .modal-container')).toBeVisible()
-    await page.locator('.kong-ui-entity-delete-modal [data-testid="modal-action-button"]').click()
+    await autocompleteDeleteModal(page)
     await waitAndDismissToasts(page)
     await expect(page.locator('.table-empty-state')).toBeVisible()
   })
