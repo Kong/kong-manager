6c687f8815783a6bb1ba291cc8114576c8a3ef26
Zehao <zehao93@outlook.com>
2026-09-08 16:39:50 +0800

    Revert "feat: freeform plugin form (#645)"
    This reverts commit 12c737a8ebd238822bf716472163676e857ad9b9.

--- tests/playwright/commands/expandAdvancedFields.ts
+++ tests/playwright/commands/expandAdvancedFields.ts
@@ -2,12 +2,16 @@ import type { Page } from '@playwright/test'
 
 // This command expands advanced fields in plugin form
 export const expandAdvancedFields = async (page: Page) => {
-  await page.locator('.entity-form .ff-plugin-configuration-form').waitFor({ state: 'attached' })
-
-  const advancedFieldsExpandTrigger = page.locator('[data-testid="ff-advanced-fields-container"] [data-testid="collapse-trigger-content"]')
+  await page
+    .locator('.entity-form > .vue-form-generator > fieldset')
+    .or(page.locator('.entity-form .k-tabs')) // for oidc plugin
+    .waitFor({ state: 'attached' })
+  const advancedFieldsExpandTrigger = page.locator('.k-collapse.nested-collapse .collapse-trigger-icon.chevron-right-icon')
 
   if (await advancedFieldsExpandTrigger.isVisible()) {
-    if ((await advancedFieldsExpandTrigger.getAttribute('aria-expanded')) !== 'true') {
+    const classes = await advancedFieldsExpandTrigger.getAttribute('class') ?? ''
+
+    if (!classes.includes('collapse-expanded')) {
       await advancedFieldsExpandTrigger.click()
     }
   }
